f25fe2bb311ef2ec1952aa545db30af33df00b62
Lisa Chen <lisachen011299@gmail.com>
2024-03-07 18:04:26 +0000

    adding cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from app.api.places import places_bp
 from app.api.reviews import reviews_bp
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object('app.config.config.ProductionConfig')
 
     app.register_blueprint(places_bp, url_prefix='/api')
